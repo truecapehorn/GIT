6eee176a31223ed1f329394922b08891036a54cb
truecapehorn <truecapehorn@gmail.com>
2018-01-30 14:55:23 +0100

    zmina krzywa

--- testy/krzywaMieszacz.py
+++ testy/krzywaMieszacz.py
@@ -12,29 +12,24 @@ while x > -31.0*wsp:
 
 def func(x):
 
-    c = 30.0*wsp # temp dla 0 stopni
-    i=0
-    p=0
-    q=0
-    while p!=-30 and q!=40 and c<40:
 
-        c+=0.1
+        c=30.0*wsp
         x1 = 20.0*wsp
-        y1 = 20.0*wsp
+        y1 =12*wsp
         x2 = -30.0*wsp
         y2 = 40.0*wsp
+
         a = (y2 - (y1 * x2 / x1) + (c * x2 / x1) - c) / (((1 * (x2**2)) - ((1 * (x1**2) * x2) / x1)))
         b = y1 / x1 - c / x1 - (a * (x1**2)) / x1
+
         fun = a * x**2 + b * x + c
 
         p=-b/(2*a)
         q=(-b**2+4*a*c)/(4*a)
-        print(c)
+        if x==20:
+            print('f(x)=({0})x**2 + ({1})x + ({2})\nWierzcholek: ({3},{4})'.format(a, b, c, p, q))
 
-    print('f(x)=({0})x**2 + ({1})x + ({2})\nWierzcholek: ({3},{4})'.format(a, b, c, p, q))
-    #if x<-25:fun=42
-
-    return fun,
+        return fun,
 
 Tco = list(map(func, tzew))
 plt.plot(tzew, Tco)
