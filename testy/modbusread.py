4695b38b9f3e3d8b180d80047fbf6fe7f59d1f6e
truecapehorn <truecapehorn@gmail.com>
2018-06-23 08:52:59 +0200

    'modbus rtu adresy'

--- testy/modbusread.py
+++ testy/modbusread.py
@@ -1,30 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import time
 import sys
 
-# TODO Dodac zapisywanie zmian w pliku.
-# TODO Opisac program w funckcji lub klasie
-start = 29 - 1
-count = 2
-unitOld = 1
-unitNew = 2
+first_arg = int(sys.argv[1])
+sec_arg = int(sys.argv[2])
+third_arg = int(sys.argv[3])
 
-client = ModbusClient(method='rtu', port='/dev/ttyUSB0', baudrate=2400, stopbits=1, parity='N', bytesize=8, timeout=3)
-connection = client.connect()
-print(connection)
-try:
-    rr = client.read_holding_registers(start, count, unit=unitOld)
-    if rr.registers[1] == 2:
-        predkosc = 2400
-    else:
-        predkosc = 'Nie 2400'
-    print('Adres urzadzenia {},\nPredkosc jest ustawiona na {},'.format(rr.registers[0], predkosc))
-    while True:
-        massure = client.read_holding_registers(6, 4, unit=unitOld)
-        print(massure.registers[0:])
-        time.sleep(1)
 
-except AttributeError:
-    print('Adres jest inny niż {} , lub jest inny błąd w adresacji '.format(unitOld))
+def adresCheck(add1=first_arg, add2=sec_arg, speed=third_arg):
+    unit_range = (add1, add2)
+    print('Sprawdzenie adresow z zakresu :', unit_range, 'Predkosc', speed, '\n')
+    count = 0
+    # unit_dymision = list(range(1, 3))
+    # print(unit_dymision)
 
-client.close()
+    client = ModbusClient(method='rtu', port='/dev/ttyUSB0', baudrate=speed, stopbits=1, parity='N', bytesize=8,
+                          timeout=3)
+
+    try:
+        while True:
+            connection = client.connect()
+            print('Polaczenie: ', count, connection)
+            for i in unit_range:
+                massure = client.read_holding_registers(6, 4, unit=i)
+                print("Wartosci dla adresu {} : {}".format(i, massure.registers[0:]))
+                time.sleep(1)
+            count += 1
+            client.close()
+
+    except AttributeError:
+        print('Adres jest inny niż {} , lub jest inny błąd w adresacji '.format(unitOld))
+
+    client.close()
+
+
+if __name__ == "__main__":
+    adresCheck()
