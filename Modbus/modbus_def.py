a276fc60907209aff583e31f87c6bab1eeda9a7e
truecapehorn <truecapehorn@gmail.com>
2018-07-04 19:14:04 +0200

    wybranie portu w modbus defy

--- Modbus/modbus_def.py
+++ Modbus/modbus_def.py
@@ -19,10 +19,31 @@ def port():
 
 
 
+def port_v2():
+    port='non'
+    ktoryPort = input('Jaki Port chesz wybrać\n1: dev/ttyUSB0\n2: com2\n3: com3\n4: com4\n5: com5\n')
+    if ktoryPort =='1':
+        port = '/dev/ttyUSB0'
+    elif ktoryPort=='2':
+        port = 'com2'
+    elif ktoryPort=='3':
+        port = 'com3'
+    elif ktoryPort=='4':
+        port = 'com4'
+    elif ktoryPort=='5':
+        port = 'com5'
+    print(port)
+    return port
+
+
+
+
+
+
+
 if __name__ == '__main__':
 
-    print('to jest to')
-    print('\rco jest i\ntak mialo byc')
+    port_v2()
 
 
 
