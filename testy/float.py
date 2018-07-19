26db98c70dafd5c89a529061f41f901d413dd285
truecapehorn <truecapehorn@gmail.com>
2018-07-19 21:55:37 +0200

    'traugutta modbus api'

--- testy/float.py
+++ testy/float.py
@@ -1,7 +1,13 @@
 import numpy as np
-data_bytes = np.array([0x64, 0xD8, 0x6E, 0x3F,0x64, 0xD8, 0x6E, 0x3F], dtype=np.uint16)
+data_bytes = np.array([0x64, 0xD8, 0x6E, 0x3F,0x64, 0xD8], dtype=np.uint16)
 data_as_float = data_bytes.view(dtype=np.float32)
+
 print(data_as_float)
+print(type(data_as_float))
+list=data_as_float.tolist()
+print(list)
+print(type(list))
+
 
 
 
