765b5fece1aa485b1904ee97e20603aeea81b420
truecapehorn <truecapehorn@gmail.com>
2018-07-24 15:03:36 +0200

    dodanie opcji ADDRES CHANGE

--- Modbus/API/modbus_master.py
+++ Modbus/API/modbus_master.py
@@ -1,25 +1,38 @@
-import sys
+
 from modbus_rtu_v3 import Api
 import argparse
-from ast import literal_eval
+
+'''
+Main program do osblugi Api dla modbasa rtu.
+Musi sie znajdowac w tym samym folderze co modbus_rtu_v3
+
+Za pomoca parsera odczytuje opcje wprowadzane w terminal. 
+
+Pomoc dla opcji po wpisaniu w terminal:
+
+python modbus_master.py -h
+
+!!!!Wersja zmiany adresu dla czujnikow appr!!!!
+
+
+'''
+
 
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-b', action='store', dest='speed', default=9600,
-                    help='Przechowuje predkosc polaczenia')
+                    help='Predkość połączenia def: 9600')
 
 parser.add_argument('-u', action='append', dest='unit',
                     default=[],
-                    help='Dodanie unit adress ex:(-u 1 -u 2 -3)',)
+                    help='Adres Modbus ex:(-u 1 -u 2 -3)', )
 
 parser.add_argument('-s', action='store', dest='reg_start',
                     help='Start zapytania')
 
-
 parser.add_argument('-l', action='store', dest='reg_lenght',
-                    help='Długośc zapytania')
-
+                    help='Długość zapytania')
 
 parser.add_argument('-i', action='store_const', dest='data_type',
                     const='ui16',
@@ -29,54 +42,51 @@ parser.add_argument('-f', action='store_const', dest='data_type',
                     const='float',
                     help='Rejetry typu float')
 
-
 parser.add_argument('-q', action='store', dest='qty', default=3,
-                    help='Przechowuje ilosc powtorzen')
+                    help='Ilosc powtorzen def: 3')
+
+parser.add_argument('-a', action='store_true', default=False,
+                    dest='boolean_switch',
+                    help='Uruchominie zmiany adresu dla appar')
+
+parser.add_argument('-o', action='store', dest='unit_old',
+                    help='Stary unit adres ')
+
+parser.add_argument('-n', action='store', dest='unit_new',
+                    help='Nowy unit adres')
 
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
+results = parser.parse_args() # pobranie rezultatow parsera
+add_change = results.boolean_switch # sprawdzenie co chemy zrobic ( odczytanie rej, czy zmiana adresu )
 
-results = parser.parse_args()
-units=[]
-for i in results.unit:
-    units.append(int(i))
+if add_change == False:
 
+    units = []
+    for i in results.unit:
+        units.append(int(i))
 
-print ('speed     =', results.speed)
-print ('unit       =', units)
-print ('reg_start       =', results.reg_start)
-print ('reg_lenght       =', results.reg_lenght)
-print ('typ rejestru   =', results.data_type)
-print ('qty     =', results.qty)
+    print(
+        "\n     Data:\nspeed = {},\nunit = {},\nreg_start= {},\nreg_lenght= {},\nreg_type= {},\nqty= {},".format(
+            results.speed, units,
+            results.reg_start,
+            results.reg_lenght,
+            results.data_type,
+            results.qty))
 
-
-
-speed=results.speed
-unit=units
-reg_start=results.reg_start
-reg_lenght=results.reg_lenght
-data_type=results.data_type
-qty=results.qty
-
-rtu=Api(speed=speed)
-
-if data_type == 'ui16':
-    readholding = rtu.read_holding(unit, reg_start, reg_lenght, data_type, qty)
-elif data_type == 'float':
-    readinput = rtu.read_input(unit, reg_start, reg_lenght, data_type, qty)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    speed = int(results.speed)
+    unit = units
+    reg_start = int(results.reg_start)
+    reg_lenght = int(results.reg_lenght)
+    data_type = results.data_type
+    qty = int(results.qty)
+    rtu = Api(speed=speed)
+    if data_type == 'ui16':
+        readholding = rtu.read_holding(unit, reg_start, reg_lenght, data_type, qty)
+    elif data_type == 'float':
+        readinput = rtu.read_input(unit, reg_start, reg_lenght, data_type, qty)
+else:
+    unit_old = int(results.unit_old)
+    unit_new = int(results.unit_new)
+    rtu = Api(speed=9600)
+    add = rtu.appar_add_change(unit_old, unit_new, 'ui16')
--- Modbus/API/modbus_rtu_v3.py
+++ Modbus/API/modbus_rtu_v3.py
@@ -13,7 +13,7 @@ import numpy as np
 class Api():
     ''' Obsłoga Modbus RTU'''
 
-    def __init__(self, method='rtu', port='com3', speed=9600, stopbits=2, parity='N', bytesize=8, timeout=1):
+    def __init__(self, method='rtu', port='com2', speed=9600, stopbits=2, parity='N', bytesize=8, timeout=1):
         self.method = method
         self.port = port
         self.speed = speed
@@ -27,7 +27,7 @@ class Api():
                               parity=self.parity, bytesize=self.bytesize, timeout=self.timeout)
 
         print(
-            "Odczyt\nmethod = {},\nport = {},\nbaudrate = {},\nstopbits = {},\nparity = {},\nbytesize = {},\ntimeout = {},\n".format(
+            "   Connection:\nmethod = {},\nport = {},\nbaudrate = {},\nstopbits = {},\nparity = {},\nbytesize = {},\ntimeout = {},\n".format(
                 self.method,
                 self.port,
                 self.speed,
@@ -55,13 +55,15 @@ class Api():
                     client.connect()
                     massure = client.read_holding_registers(reg_start, reg_lenght, unit=i)
                     if data_type == 'ui16':
-                        print('Rejestry {} dla adresu {}: {}'.format(data_type,i, massure.registers[0:]))
+                        data = massure.registers[0:]
+                        print('Rejestry {} dla adresu {}: {}'.format(data_type, i, data))
                     elif data_type == 'float':
                         massure.registers[0::2], massure.registers[1::2] = massure.registers[1::2], massure.registers[
                                                                                                     0::2]
                         data_arr = np.array([massure.registers[0:]], dtype=np.int16)
                         data_as_float = data_arr.view(dtype=np.float32)
-                        print('Rejestry {} dla adresu {}: {}'.format(data_type,i, data_as_float.tolist()))
+                        data = data_as_float
+                        print('Rejestry {} dla adresu {}: {}'.format(data_type, i, data))
                     client.close()
                 except AttributeError:
                     print('Połaczenie z adresem {} nie udane'.format(i))
@@ -73,34 +75,35 @@ class Api():
 
             sesion += 1
             print("\n")
+        return data
 
     def read_input(self, unit, reg_start, reg_lenght, data_type, qty=5):
         client = self.connection()
         connection = client.connect()
-        if data_type=='float':
-            reg_lenght+=2
+        if data_type == 'float':
+            reg_lenght += 2
         print("Odczyt adresow input reg od {} do {} dla urzadzen {} : {}".format(reg_start,
-                                                                                    (reg_start + reg_lenght),
-                                                                                    unit,
-                                                                                    connection))
+                                                                                 (reg_start + reg_lenght),
+                                                                                 unit,
+                                                                                 connection))
         sesion = 1
         while connection and sesion <= qty:
             print("Sesja nr: ", sesion)
             time.sleep(0.3)
-            massure=[]
+            massure = []
             for i in unit:
                 try:
                     client.connect()
                     massure = client.read_input_registers(reg_start, reg_lenght, unit=i)
 
                     if data_type == 'ui16':
-                        print('Rejestry {} dla adresu {}: {}'.format(data_type,i, massure.registers[0:]))
+                        print('Rejestry {} dla adresu {}: {}'.format(data_type, i, massure.registers[0:]))
                     elif data_type == 'float':
                         massure.registers[0::2], massure.registers[1::2] = massure.registers[1::2], massure.registers[
                                                                                                     0::2]
                         data_arr = np.array([massure.registers[0:]], dtype=np.int16)
                         data_as_float = data_arr.view(dtype=np.float32)
-                        print('Rejestry {} dla adresu {}: {}'.format(data_type,i, data_as_float.tolist()))
+                        print('Rejestry {} dla adresu {}: {}'.format(data_type, i, data_as_float.tolist()))
                     client.close()
                 except AttributeError:
                     print('Połaczenie z adresem {} nie udane'.format(i))
@@ -113,15 +116,43 @@ class Api():
             sesion += 1
             print("\n")
 
+    def write_register(self, reg_add, val, mod_adress):
+        try:
+            client = self.connection()
+            connection = client.connect()
+            time.sleep(0.3)
+            client.write_register(reg_add, val, unit=mod_adress)
+            client.close()
+        except AttributeError:
+            print('Połaczenie z adresem {} nie udane'.format(mod_adress))
+        return print('Nowa wartosc zapisana')
+
+    def appar_add_change(self, unitOld, unitNew, data_type):
+        unit = []
+        unit.append(unitOld)
+        print(unit)
+        reg = self.read_holding(unit, 0, 30, data_type, 5)
+        if reg != None:
+            testVar = input("Czy chcesz zmienic adres z {} na {} ( t/n).".format(unitOld, unitNew))
+            if testVar == "t" or testVar == 'T' and reg[1][28] == unitOld:
+                print('Zmieniam adres z {} na {}'.format(unitOld, unitNew))
+                self.write_register(28, unitNew, unitOld)
+                time.sleep(0.5)
+                unit = []
+                print('Przeprowadzono zmiane adresu z {} na {}'.format(unitOld, unitNew))
+                unit.append(unitNew)
+                self.read_holding(unit, 0, 30, data_type, 5)
+        print('\n')
 
 
 if __name__ == '__main__':
     rtu = Api()
-    fifek=Api(speed=2400)
+    fifek = Api(speed=2400)
     print(Api.__doc__)
-    unith = [3,5,7,9,10]
+    unith = [3]
     uniti = [64]
-    fif=[1]
-    readholding = rtu.read_holding(unith, 0, 10, 'ui16', 5)
-    readinput1 = rtu.read_input(uniti, 0, 10, 'ui16', 5)
-    #readinput2 = fifek.read_input(fif, 0, 70, 'float', 5)
+    fif = [1]
+    readholding = rtu.read_holding(unith, 0, 10, 'ui16', 10)
+    # readinput1 = rtu.read_input(uniti, 0, 10, 'ui16', 5)
+    # readinput2 = fifek.read_input(fif, 0, 70, 'float', 5)
+    #writereg = rtu.appar_add_change(1, 11, 'ui16')
