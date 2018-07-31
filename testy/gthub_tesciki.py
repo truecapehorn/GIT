e2de72c31881a34df3e9463290284ef05ad2aada
truecapehorn <truecapehorn@gmail.com>
2018-07-31 09:00:52 +0200

    zapisywanie do listingu daty i godziny'

--- testy/gthub_tesciki.py
+++ testy/gthub_tesciki.py
@@ -2,7 +2,18 @@
 
 import datetime,time
 
+import sys
+filename=sys.argv[0]
+logi = open('{}'.format(filename), 'a', encoding='utf8')
 now = datetime.datetime.now()
 print('Teraz jest godzina : ', now)
+print('"""Uruchomienie programu było o godzinie : {}"""\n'.format(now), file=logi)
+logi.close()
 time.sleep(2)
-''' dodanie commita'''
+
+"""Uruchomienie programu było o godzinie : 2018-07-31 08:59:41.811608"""
+
+"""Uruchomienie programu było o godzinie : 2018-07-31 08:59:47.255920"""
+
+"""Uruchomienie programu było o godzinie : 2018-07-31 08:59:59.681630"""
+
