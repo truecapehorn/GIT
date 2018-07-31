5c32b2b64697d7a74e13f4af09afb62f0d057524
truecapehorn <truecapehorn@gmail.com>
2018-07-31 09:17:24 +0200

    bardziej nowszy commmit

--- testy/gthub_tesciki.py
+++ testy/gthub_tesciki.py
@@ -21,3 +21,11 @@ time.sleep(2)
 
 """Uruchomienie programu było o godzinie : 2018-07-31 09:16:16.349493"""
 
+"""Uruchomienie programu było o godzinie : 2018-07-31 09:16:58.734917"""
+
+"""Uruchomienie programu było o godzinie : 2018-07-31 09:17:00.343009"""
+
+"""Uruchomienie programu było o godzinie : 2018-07-31 09:17:03.237174"""
+
+"""Uruchomienie programu było o godzinie : 2018-07-31 09:17:08.807493"""
+
