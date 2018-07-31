b37ab8299fc98287624e32c5541d1c3673fd61d6
truecapehorn <truecapehorn@gmail.com>
2018-07-31 10:50:58 +0200

    nowy branch

--- testy/gthub_tesciki.py
+++ testy/gthub_tesciki.py
@@ -13,23 +13,13 @@ time.sleep(2)
 
 """Uruchomienie programu było o godzinie : 2018-07-31 08:59:41.811608"""
 
-"""Uruchomienie programu było o godzinie : 2018-07-31 08:59:47.255920"""
 
-"""Uruchomienie programu było o godzinie : 2018-07-31 08:59:59.681630"""
 
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:01:28.010683"""
+"""Uruchomienie programu było o godzinie : 2018-07-31 09:47:54.108038"""
 
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:16:16.349493"""
+"""Uruchomienie programu było o godzinie : 2018-07-31 09:48:04.376626"""
 
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:16:58.734917"""
+"""Uruchomienie programu było o godzinie : 2018-07-31 10:50:14.463974"""
 
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:17:00.343009"""
-
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:17:03.237174"""
-
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:17:08.807493"""
-
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:20:17.151266"""
-
-"""Uruchomienie programu było o godzinie : 2018-07-31 09:24:40.404323"""
+"""Uruchomienie programu było o godzinie : 2018-07-31 10:50:16.625098"""
 
